6580a8ea46524e90dcd89c8b1ca1122bf6f2028a
dhruv-gundecha <dhruv.gundecha@gmail.com>
2023-11-01 16:52:53 +0530

    final commit hopefully

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:max/pallete.dart';
+import 'package:flutter/material.dart';
+import 'package:max/signin_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Max',
+      theme: ThemeData.light(useMaterial3: true).copyWith(
+        scaffoldBackgroundColor: Pallete.whiteColor,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Pallete.whiteColor,
+        ),
+      ),
+      home: const SignInScreen(),
+    );
+  }
+}
